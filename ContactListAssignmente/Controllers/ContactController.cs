03f9c4e6e1c0b35578573f8f2e5f3110920c5a7d
motty roth <mottyroth1@gmail.com>
2021-08-31 12:05:33 -0400

    inital commit(besides gitignore)

---
+++ ContactListAssignmente/Controllers/ContactController.cs
@@ -0,0 +1,64 @@
+﻿using ContactListAssignmente.Models;
+using ContactListAssignmente.Utils;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactListAssignmente.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly ContactsHandler _contactsHandler;
+        public ContactController()
+        {
+            _contactsHandler = ContactsHandler.GetInstance();
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Contact contact)
+        {
+            _contactsHandler.Add(contact);
+
+            return RedirectToAction(nameof(List));
+        }
+
+        //this should've used the httpDelete method but uses get method instead, so that it should be able to be used as an href without js
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            _contactsHandler.Remove(id);
+
+            return RedirectToAction(nameof(List));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.ContactId = id;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Contact contact)
+        {
+            _contactsHandler.Edit(contact);
+            
+            return RedirectToAction(nameof(List)); 
+        }
+    }
+}
---
+++ ContactListAssignmente/Utils/ContactsHandler.cs
@@ -0,0 +1,59 @@
+﻿using ContactListAssignmente.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ContactListAssignmente.Utils
+{
+    public class ContactsHandler
+    {
+        private readonly List<Contact> _contacts;
+        private static ContactsHandler _contactsHandler;
+        public int Count { get => _contacts.Count; }
+        private int _idCounter;
+        
+        private ContactsHandler()
+        { 
+            _contacts = new List<Contact>();
+            _idCounter = 0;
+        }
+
+        public static ContactsHandler GetInstance()
+        {
+            return _contactsHandler ??= new ContactsHandler();
+        }
+
+        public void Add(Contact contact)
+        {
+            contact.Id = _idCounter++;
+            _contacts.Add(contact);
+        }
+
+        public bool Remove(int id)
+        {
+            return _contacts.Remove(_contacts.Find(x => x.Id == id));
+        }
+
+        public bool Edit(Contact contact)
+        {
+            bool removed = Remove(contact.Id);
+
+            if (removed)
+                _contacts.Add(contact);
+
+            return removed;
+        }
+
+        public IEnumerable<Contact> All()
+        {
+            return _contacts;
+        }
+
+        public Contact Find(int id)
+        {
+            return _contacts.First(x => x.Id == id);
+        }
+    }
+}
